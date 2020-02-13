9cfcb5fc616a66b9fb5aec7cd0a4b2b376fc37d1
Theo O Rashid <tar15@wpea-101748-ws>
2020-02-13 15:13:38 +0000

    Spatial join not working

--- intersect.py
+++ intersect.py
@@ -8,6 +8,7 @@ import os
 import pandas as pd
 import numpy as np
 import geopandas as gpd
+import functools
 import matplotlib.pyplot as plt
 
 __author__ = "Theo Rashid"
@@ -15,3 +16,6 @@ __email__ = "tar15@ic.ac.uk"
 
 pc_df   = gpd.read_file("../Data/ldn_postcode_shape/Pc2011_810111.shp")
 ward_df = gpd.read_file("../Data/ldn_Ward98_shape/Ward98.shp")
+
+# Find which postcodes are in each Ward
+gpd.sjoin(pc_df, ward_df, how="inner", op="intersects") # not working
