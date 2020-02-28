7ae88d9d3534997e4623c98588514f5552d53ba8
Theo O Rashid <tar15@wpea-101748-ws>
2020-02-28 14:27:57 +0000

    added 0 and 1-4 binning

--- population.py
+++ population.py
@@ -17,26 +17,18 @@ __email__ = "tar15@ic.ac.uk"
 males_dfs = []
 females_dfs = []
 
-# # 2001 census populations are grouped by 5 year age group but at COA level
-# # Need to group by LSOA level
-# pop_2001_df = pd.read_csv("../Data/Populations/pop2001_coa11.csv", engine = "python")
-# geog_2011 = pd.read_csv("../Data/Geography/OA2011_lookup.csv", engine = "python")
-# geog_2011 = geog_2011[~geog_2011["LSOA11CD"].astype(str).str.startswith('S')] # Remove scottish geographies
-# pop_2001_df = pop_2001_df.merge(geog_2011, left_on="coa11", right_on="OA11CD").groupby("LSOA11CD").sum()
-# # DISCREPANCY BETWEEN 2001 and 2011 OA/LSOA numbers
-
 # 2002-2011 is split into 4 files grouped in age by year
-males_2002_2010_prep_dfs = []
-females_2002_2010_prep_dfs = []
+males_2002_2011_prep_dfs = []
+females_2002_2011_prep_dfs = []
 
 for year in range(2002, 2006+1):
-    males_2002_2010_prep_dfs.append(pd.read_excel("../Data/Populations/pop2002-2006_lsoa2011_male.xls", sheet_name = "Mid-{0!s}".format(year),  engine = "xlrd"))
-    females_2002_2010_prep_dfs.append(pd.read_excel("../Data/Populations/pop2002-2006_lsoa2011_female.xls", sheet_name = "Mid-{0!s}".format(year),  engine = "xlrd"))
-for year in range(2007, 2010+1):
-    males_2002_2010_prep_dfs.append(pd.read_excel("../Data/Populations/pop2007-2010_lsoa2011_male.xls", sheet_name = "Mid-{0!s}".format(year),  engine = "xlrd"))
-    females_2002_2010_prep_dfs.append(pd.read_excel("../Data/Populations/pop2007-2010_lsoa2011_female.xls", sheet_name = "Mid-{0!s}".format(year),  engine = "xlrd"))
+    males_2002_2011_prep_dfs.append(pd.read_excel("../Data/Populations/unclean populations/pop2002-2006_lsoa2011_male.xls", sheet_name = "Mid-{0!s}".format(year),  engine = "xlrd"))
+    females_2002_2011_prep_dfs.append(pd.read_excel("../Data/Populations/unclean populations/pop2002-2006_lsoa2011_female.xls", sheet_name = "Mid-{0!s}".format(year),  engine = "xlrd"))
+for year in range(2007, 2011+1):
+    males_2002_2011_prep_dfs.append(pd.read_excel("../Data/Populations/unclean populations/pop2007-2010_lsoa2011_male.xls", sheet_name = "Mid-{0!s}".format(year),  engine = "xlrd"))
+    females_2002_2011_prep_dfs.append(pd.read_excel("../Data/Populations/unclean populations/pop2007-2010_lsoa2011_female.xls", sheet_name = "Mid-{0!s}".format(year),  engine = "xlrd"))
 
-def clean_pop_2002_2010(df, gender):
+def clean_pop_2002_2011(df, gender):
     """Function to clean dataframes from 2002 - 2011 format
 
     Keyword arguments:
@@ -46,66 +38,51 @@ def clean_pop_2002_2010(df, gender):
 
     offset = 4 # first 4 rows do not contain information about group per age
 
-    lower = 0
-    upper = 4 # 5 year age groups
+    # Need a 0 age bin, and a 1-4 age bin
+    df[gender + "00"] = df.iloc[:,0+offset]
+    df[gender + "1_4"] = df.iloc[:, 1+offset:4+offset+1].sum(axis=1)
+    lower = 5 # First 5 year age bin is 5-9
+    upper = 9 # 5 year age groups
     while upper < 85:
         df[gender + str(lower) + "_" + str(upper)] = df.iloc[:, lower+offset:upper+offset+1].sum(axis=1)
         lower += 5
         upper += 5
     df[gender + "85+"] = df.iloc[:, 85+offset:90+offset+1].sum(axis=1) # separate for 85+ column
     df = df.drop(df.iloc[:,1:95], axis = 1)
-    df = df.rename(columns={"LSOA11CD": "LSOA2011"})
+    df = df.rename(columns={"LSOA11CD": "LSOA2011", gender + "00": gender + "0"})
     return df
 
-for i in range(len(range(2002, 2010+1))):
-    male_df   = clean_pop_2002_2010(males_2002_2010_prep_dfs[i], "m")
+for i in range(len(range(2002, 2011+1))):
+    male_df   = clean_pop_2002_2011(males_2002_2011_prep_dfs[i], "m")
     males_dfs.append(male_df)
-    female_df = clean_pop_2002_2010(females_2002_2010_prep_dfs[i], "f")
+    female_df = clean_pop_2002_2011(females_2002_2011_prep_dfs[i], "f")
     females_dfs.append(female_df)
-
-# 2011 census populations are grouped by 5 year age group but the 90+ needs to be changed to 85+
-males_df_2011 = pd.read_excel("../Data/Populations/pop2011_lsoa2011.xls", sheet_name = "Mid-2011 Males",  engine = "xlrd", skiprows = 3).dropna(subset=["Unnamed: 2"])
-females_df_2011 = pd.read_excel("../Data/Populations/pop2011_lsoa2011.xls", sheet_name = "Mid-2011 Females",  engine = "xlrd", skiprows = 3).dropna(subset=["Unnamed: 2"])
-
-area_codes = males_df_2011["Area Codes"]
-
-males_df_2011["85+"] = males_df_2011.iloc[:,-2] + males_df_2011.iloc[:,-1]
-males_df_2011 = males_df_2011.drop(columns=["Area Codes", "Area Names", "Unnamed: 2", "All Ages", "85-89", "90+"])
-males_df_2011 = males_df_2011.add_prefix('m')
-males_df_2011.columns = males_df_2011.columns.str.replace("-", "_")
-males_df_2011.insert(0, "LSOA2011", area_codes)
-males_dfs.append(males_df_2011)
-
-females_df_2011["85+"] = females_df_2011.iloc[:,-2] + females_df_2011.iloc[:,-1]
-females_df_2011 = females_df_2011.drop(columns=["Area Codes", "Area Names", "Unnamed: 2", "All Ages", "85-89", "90+"])
-females_df_2011 = females_df_2011.add_prefix('f')
-females_df_2011.columns = females_df_2011.columns.str.replace("-", "_")
-females_df_2011.insert(0, "LSOA2011", area_codes)
-females_dfs.append(females_df_2011)
 
 # Files between 2012-2017 are of the same file type with a different sheet for males and females and a population estimate for each age until 90+
 for year in range(2012, 2017+1):
-    males_df = pd.read_excel("../Data/Populations/pop{0!s}_lsoa2011.xls".format(year), sheet_name = "Mid-{0!s} Males".format(year),  engine = "xlrd", skiprows = 4)
+    males_df = pd.read_excel("../Data/Populations/unclean populations/pop{0!s}_lsoa2011.xls".format(year), sheet_name = "Mid-{0!s} Males".format(year),  engine = "xlrd", skiprows = 4)
     males_df = males_df.dropna(subset=["Unnamed: 2"])
     males_df = males_df.rename(columns={"Area Codes": "LSOA2011"})
-    females_df = pd.read_excel("../Data/Populations/pop{0!s}_lsoa2011.xls".format(year), sheet_name = "Mid-{0!s} Females".format(year),  engine = "xlrd", skiprows = 4)
+    females_df = pd.read_excel("../Data/Populations/unclean populations/pop{0!s}_lsoa2011.xls".format(year), sheet_name = "Mid-{0!s} Females".format(year),  engine = "xlrd", skiprows = 4)
     females_df = females_df.dropna(subset=["Unnamed: 2"])
     females_df = females_df.rename(columns={"Area Codes": "LSOA2011"})
 
-    # Sum into five year age groups (and 85+)
-    age_groups_list = np.array_split(range(85), 17) # splits into groups of 5 years
+    # Sum into five year age groups (and 0, 1-4, 85+)
+    age_groups_list = np.array_split(range(85), 17)[1:] # splits into groups of 5 years from 5 to 85
 
+    males_df["m0"] = males_df.loc[:, [0]].sum(axis=1)
+    males_df["m1_4"] = males_df.loc[:, [1, 2, 3, 4]].sum(axis=1)
     for group in age_groups_list:
         males_df["m" + str(min(group)) + "_" + str(max(group))] = males_df.loc[:, group].sum(axis=1)
     males_df["m85+"] = males_df.loc[:, [85, 86, 87, 88, 89, "90+"]].sum(axis=1) # separate for 85+ column
-
     males_df.drop(males_df.columns[1:95], axis=1, inplace=True) # Keep only the LSOA code and summed columns
     males_dfs.append(males_df)
 
+    females_df["f0"] = females_df.loc[:, [0]].sum(axis=1)
+    females_df["f1_4"] = females_df.loc[:, [1, 2, 3, 4]].sum(axis=1)
     for group in age_groups_list:
         females_df["f" + str(min(group)) + "_" + str(max(group))] = females_df.loc[:, group].sum(axis=1)
     females_df["f85+"] = females_df.loc[:, [85, 86, 87, 88, 89, "90+"]].sum(axis=1) # separate for 85+ column
-
     females_df.drop(females_df.columns[1:95], axis=1, inplace=True) # Keep only the LSOA code and summed columns
     females_dfs.append(females_df)
 
