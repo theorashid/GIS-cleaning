d67e781c5caffb801d3b88f8830c8f95c7ef986b
theorashid <30835680+theorashid@users.noreply.github.com>
2020-04-07 12:14:11 +0100

    improved simulation parameters
    More realistic death rates

--- simulate.R
+++ simulate.R
@@ -1,10 +1,15 @@
 
-# Theo AO Rashid -- February 2020
+# Theo AO Rashid -- April 2020
 
 # ----- Simulate death data -----
 # Build model from the death rates and add a term proportional
-# to LSOA's income deprivation. This should have some spatial
-# component. Add some random noise also
+# to LSOA's income deprivation.
+#
+# Poisson death rate model with:
+# - Age terms from UK study (Bennett et al. 2015)
+# - Temporal component from ONS mortality rates
+# - Spatial component from income deprivation (1/4 scale of age effects)
+# - Random noise (1/10 scale of age effects)
 
 library(dplyr)
 library(tidyr)
@@ -22,17 +27,25 @@ pop_IMD_df_wide <- pop_IMD_df %>%
 
 populations <- data.matrix(pop_IMD_df_wide[9:27]) # dim (135380, 19)
 income_dep <- data.matrix(pop_IMD_df_wide[7]) # mean 0.1852
+years <- data.matrix(pop_IMD_df_wide[4])
 
 # Rough estimated death rates for 19 age groups
 mxE <- exp(-5.5 + c(0,-2.9,-3,-2.9,-1.8,-1.7,-1.7,-1.6,-1.4,-.8,-.2,.3,1.0,1.5,1.9,2.4,2.8,3.4,3.8)) # mean 0.0260, sd 0.0491
 mxE_mat <- t(replicate(135380, mxE)) # expand this into the length of populations/income deprivation
 
+# Temporal term based on slope of ONS mortality rates 2001 and 2019
+beta <- (1229.8/100000 - 919.9/100000)/(2019 - 2001)
+time_effects <- beta * (years - mean(years))/sum(abs(range(years - mean(years)))) # recentred and scaled years
+mxE_mat <- mxE_mat + matrix(rep(time_effects, 19), ncol=19)
+
 # Add a term proportional to income deprivation
-mxE_mat <- mxE_mat + matrix(rep(income_dep, 19), ncol=19) * 0.3
+scaled_income_mat <- matrix(rep(scale(income_dep), 19), ncol=19)
+mxE_mat <- mxE_mat + sweep(scaled_income_mat, MARGIN=2, mxE/4, `*`) # income term of 1/4 the order of age effects
 
 # Add some random normal noise
-noise <- matrix(rnorm(2572220), 135380) * 0.001
+noise <- sweep(matrix(rnorm(2572220), 135380), MARGIN=2, mxE/10, `*`) # noise of 1/10 the order of age effects
 mxE_mat <- mxE_mat + noise
+
 mxE_mat[mxE_mat < 0] <- 0 # cannot have negative death rates
 
 # Populations
